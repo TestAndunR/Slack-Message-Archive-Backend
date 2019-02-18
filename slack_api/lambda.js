b48a8fcaadd1b7236be7eece4ad25846a11f4d5f
TestAndunR <andunranmal@gmail.com>
2019-02-18 15:05:35 +0530

    add a ddb

--- slack_api/lambda.js
+++ slack_api/lambda.js
@@ -1,8 +1,27 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = function (event, context, callback) {
     if (event.challenge) {
-       callback(null, {"challenge": event.challenge});
-       return;
-   }
-   console.log(event)
-   callback(null, {"message":"Success"})
+        callback(null, { "challenge": event.challenge });
+        return;
+    }
+    console.log(event);
+    let messageId = event.event.client_msg_id;
+    let message = event.event.text;
+    let senderId = event.event.user;
+    let timestamp = event.event.ts
+    ddb.put({
+        TableName: 'slack_messages',
+        Item: { 'message_id': messageId, 'message': message, 'sender_Id': senderId, 'timestamp': timestamp }
+    }).promise().then(function (data) {
+        //your logic goes here
+        console.log(data);
+    }).catch(function (err) {
+        //handle error
+        console.log(err)
+    });
+
+
+    callback(null, { "message": "Success" })
 }
