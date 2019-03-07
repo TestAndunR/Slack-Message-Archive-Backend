70d1a75d03e6c0d008839ea275c275304ba41b2f
AndunRanmal <kannangarar1993@gmail.com>
2019-03-07 11:46:45 +0530

    Add console.log

--- slack_api/lambda.js
+++ slack_api/lambda.js
@@ -7,14 +7,14 @@ exports.handler = function (event, context, callback) {
         callback(null, { "challenge": event.challenge });
         return;
     }
-    // console.log(event.event);
+    console.log(event.event);
     let messageId = event.event.client_msg_id;
     let message = event.event.text !== ''? event.event.text : "no";
     let senderId = event.event.user;
     let timestamp = event.event.ts;
     let channelId = event.event.channel;
     let messageInLowerCase = event.event.text !== ''? event.event.text.toLocaleLowerCase() : "no";
-    let files = event.event.files.lenth !== 0 || undefined ? event.event.files : [];
+    let files = event.event.files.lenth !== 0 || undefined ? JSON.stringify(event.event.files) : JSON.stringify([]);
     console.log(message, messageInLowerCase, messageId, senderId, timestamp, channelId, files);
     ddb.put({
         TableName: 'slack_messages',
