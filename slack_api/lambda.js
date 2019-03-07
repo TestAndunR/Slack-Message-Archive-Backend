8dcaf746579e7da0fe8110619cef4d8800be6480
AndunRanmal <kannangarar1993@gmail.com>
2019-03-07 13:04:01 +0530

    add new delete operation for ddb

--- slack_api/lambda.js
+++ slack_api/lambda.js
@@ -7,63 +7,84 @@ exports.handler = function (event, context, callback) {
         callback(null, { "challenge": event.challenge });
         return;
     }
-    if (event.event.subtype === "message_changed") {
-        let messageId = event.event.message.client_msg_idl;
-        let message = event.event.message.text;
-        ddb.update({
-            TableName: 'slack_messages',
-            Key: {
-                'message_id': messageId
-            },
-            ExpressionAttributeNames: {
-                '#message': 'message',
-                '#messageInLowerCase': 'messageInLowerCase',
-                '#messageId': 'messageId'
-            },
-            ExpressionAttributeValues: {
-                ':message': message,
-                ':messageInLowerCase': messageInLowerCase,
-                ':messageId': messageId
-            },
-            UpdateExpression: 'set #message = :message , #messageInLowerCase = :messageInLowerCase',
-            ConditionExpression: '#messageId = :messageId'
-        }).promise().then(function (data) {
-            //your logic goes here
-            console.log(data);
-        }).catch(function (err) {
-            //handle error
-            console.log(err);
-        });
-        let messageInLowerCase = event.event.message.text.toLocaleLowerCase();
-    }
-    console.log(event.event);
-    let messageId = event.event.client_msg_id;
-    let message = event.event.text !== '' ? event.event.text : "no";
-    let senderId = event.event.user;
-    let timestamp = event.event.ts;
-    let channelId = event.event.channel;
-    let messageInLowerCase = event.event.text !== '' ? event.event.text.toLocaleLowerCase() : "no";
-    let files = event.event.files.lenth !== 0 || undefined ? JSON.stringify(event.event.files) : JSON.stringify([]);
-    console.log(message, messageInLowerCase, messageId, senderId, timestamp, channelId, files);
-    ddb.put({
-        TableName: 'slack_messages',
-        Item: {
-            'message_id': messageId,
-            'message': message,
-            'sender_Id': senderId,
-            'timestamp': timestamp,
-            'channel_Id': channelId,
-            'messageInLowerCase': messageInLowerCase,
-            'files': files
-        }
-    }).promise().then(function (data) {
-        //your logic goes here
-        console.log(data);
-    }).catch(function (err) {
-        //handle error
-        console.log(err)
-    });
 
+    let messageId;
+    let message;
+    let messageInLowerCase
+
+    switch (event.event.subtype) {
+        case "message_changed":
+            messageId = event.event.message.client_msg_id;
+            message = event.event.message.text;
+            messageInLowerCase = event.event.message.text.toLocaleLowerCase();
+            ddb.update({
+                TableName: 'slack_messages',
+                Key: {
+                    'message_id': messageId
+                },
+                ExpressionAttributeNames: {
+                    '#message': 'message',
+                    '#messageInLowerCase': 'messageInLowerCase',
+                    // '#messageId': 'message_Id'
+                },
+                ExpressionAttributeValues: {
+                    ':message': message,
+                    ':messageInLowerCase': messageInLowerCase,
+                    // ':messageId': messageId
+                },
+                UpdateExpression: 'set #message = :message , #messageInLowerCase = :messageInLowerCase',
+                // ConditionExpression: '#messageId = :messageId'
+            }).promise().then(function (data) {
+                //your logic goes here
+                console.log(data);
+            }).catch(function (err) {
+                //handle error
+                console.log(err);
+            });
+            break;
+        case ("message_deleted"):
+            messageId = event.event.previous_message.client_msg_id;
+            ddb.delete({
+                TableName: 'slack_messages',
+                Key: { 'message_id': messageId }
+            }).promise().then(function (data) {
+                //your logic goes here
+                console.log(data);
+            }).catch(function (err) {
+                //handle error
+                console.log(err);
+            });
+
+            break;
+        default:
+            console.log(event.event);
+            messageId = event.event.client_msg_id;
+            message = event.event.text !== '' ? event.event.text : "no";
+            let senderId = event.event.user;
+            let timestamp = event.event.ts;
+            let channelId = event.event.channel;
+            let messageInLowerCase = event.event.text !== '' ? event.event.text.toLocaleLowerCase() : "no";
+            let files = event.event.files.lenth !== 0 || undefined ? JSON.stringify(event.event.files) : JSON.stringify([]);
+            console.log(message, messageInLowerCase, messageId, senderId, timestamp, channelId, files);
+            ddb.put({
+                TableName: 'slack_messages',
+                Item: {
+                    'message_id': messageId,
+                    'message': message,
+                    'sender_Id': senderId,
+                    'timestamp': timestamp,
+                    'channel_Id': channelId,
+                    'messageInLowerCase': messageInLowerCase,
+                    'files': files
+                }
+            }).promise().then(function (data) {
+                //your logic goes here
+                console.log(data);
+            }).catch(function (err) {
+                //handle error
+                console.log(err)
+            });
+    }
 
     callback(null, { "message": "Success" })
 }
